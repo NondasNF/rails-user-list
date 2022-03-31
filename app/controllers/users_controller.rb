259708c07ceb6dd6eaa6697e5c128cc3dd98b90c
NondasNF <nondasnoronha@gmail.com>
2022-03-30 22:33:59 -0300

    Add rest-client fetch api initial config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  require 'rest-client'
+
+  def get_users
+    url = 'https://randomuser.me/api/?seed=giga'
+    response = RestClient.get(url)
+    @users = JSON.parse(response)
+    render json: @users
+  end
 end 
