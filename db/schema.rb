0ced53368da8a113539e703ad9efdb154959494e
NondasNF <nondasnoronha@gmail.com>
2022-03-31 10:07:34 -0300

    add schema for user to be stored

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2022_03_30_235832) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.json "data", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
